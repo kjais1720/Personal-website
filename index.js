59ccac9f06d1a598e7d832d9bc6e5399443bff10
Krituraaj Anand <kjais2000@gmail.com>
2021-08-24 14:26:17 +0530

    Rejigged the whole website

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 const animatedText=document.querySelectorAll(".animatedText")
+const nav = document.querySelector("nav")
+const navToggle = document.querySelector('.hamburger')
+
+navToggle.addEventListener('click',()=>{
+    nav.classList.toggle('active')
+    navToggle.classList.toggle('is-active')
+});
+
+var prevScrollPos = window.pageYOffset;
+
+
+window.onscroll = ()=>{
+
+    navToggle.classList.remove('is-active')
+    nav.classList.remove("active")
+
+    var currentScrollPos = window.pageYOffset;
+    if(currentScrollPos > 200){
+        nav.classList.add("showBg");
+    }
+    else{
+        nav.classList.remove("showBg");
+    }
+    if (prevScrollPos > currentScrollPos){
+        nav.style.top='0'
+    } else {
+        nav.style.top='-200px';
+    }
+    prevScrollPos = currentScrollPos
+}
+
 
 
 animatedText.forEach(text =>{
@@ -19,3 +50,4 @@ function toggle(){
         e.classList.toggle('active');
     })
 }
+
